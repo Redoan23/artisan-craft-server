bcdaf368df9bbb055fb74c3fc3078e4db304614b
Redoan23 <redoan1080@gmail.com>
2024-04-27 06:46:50 +0600

    added get operation by email

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -34,6 +34,22 @@ async function run() {
             const cursor = artCollection.find()
             const array = await cursor.toArray()
             res.send(array)
+        })
+
+        app.get('/artisan/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await artCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/artisan/arts/:email', async (req, res) => {
+            const email = req.params.email
+            const query = {email: email}
+            const cursor = await artCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+
         })
 
         app.post('/', async (req, res) => {
