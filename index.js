b0c18477a3b23b8f6591b2787cdfb0a1fa440dc7
Redoan23 <redoan1080@gmail.com>
2024-04-27 09:05:01 +0600

    added put method

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ async function run() {
             res.send(result)
         })
 
+        app.put('/artisan/:id', (req, res)=>{
+            const id= req.params.id
+            const data=req.body
+            console.log(data, id)
+        })
+
         app.delete('/artisan/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
